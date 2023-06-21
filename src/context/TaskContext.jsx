351d6adfbb257d79f76a32b8b539bf16353f8629
oscar-castillo-rayo <manuel.rayo3@gmail.com>
2023-06-21 10:36:48 -0600

    first commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect } from "react";
+import { task as data } from "../data/task";
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]); // basicamente dice copia todo lo que habia en tasks y agregale algo nuevo.
+  }
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        deleteTask,
+        createTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
